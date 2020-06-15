b1c3ac7ceec3adda85877b14cb4acac683f7c44b
anasansari157 <anasansari157@gmail.com>
2020-06-15 15:50:53 +0530

    add validators

--- controller/user.js
+++ controller/user.js
@@ -3,77 +3,107 @@ var auth = require("../util/auth");
 var { isValidUser } = require("../util/validatorModule");
 
 module.exports = {
-  registerUser: async (req, res, next) => {
-    try {
-      if (!req.body.user) {
-        return res.status(400).json({ message: "Invalid Credentials" });
-      }
-      if (!isValidUser(req.body.user)) {
-        return res.status(400).json({ message: "Wrong Input" });
-      }
-      var user = await User.create(req.body.user);
-      res.json({ user: user.format() });
-    } catch (error) {
-      next(error);
-    }
-  },
-  loginUser: async (req, res, next) => {
-    try {
-      var { user } = req.body;
-      if (!user || !user.email || !user.password) {
-        return res.status(400).json({ message: "Wrong Input" });
-      }
-      var currentUser = await User.findOne({ email: user.email });
-      if (!currentUser) {
-        return res.status(404).json({ message: "Invalid email address" });
-      }
-      var result = await currentUser.verifyPassword(user.password);
-      if (!result) {
-        return res.status(401).json({ message: "Invalid password" });
-      }
-      var token = await auth.generateJwt(currentUser, next);
-      console.log(token);
-      res.json({ user: currentUser.format(), token });
-    } catch (error) {
-      next(error);
-    }
-  },
-  updateUser: async (req, res, next) => {
-    try {
-      var user = req.body.user;
+	registerUser: async (req, res, next) => {
+		try {
+			if (!req.body.user) {
+				return res.status(400).json({ message: "Invalid Credentials" });
+			}
+			if (!isValidUser(req.body.user)) {
+				return res.status(400).json({ message: "Wrong Input" });
+			}
+			var user = await User.create(req.body.user);
+			res.json({ user: user.format() });
+		} catch (error) {
+			next(error);
+		}
+	},
+	loginUser: async (req, res, next) => {
+		try {
+			var { user } = req.body;
+			if (!user || !user.email || !user.password) {
+				return res.status(400).json({ message: "Wrong Input" });
+			}
+			var currentUser = await User.findOne({ email: user.email });
+			if (!currentUser) {
+				return res.status(404).json({ message: "Invalid email address" });
+			}
+			var result = await currentUser.verifyPassword(user.password);
+			if (!result) {
+				return res.status(401).json({ message: "Invalid password" });
+			}
+			var token = await auth.generateJwt(currentUser, next);
+			console.log(token);
+			res.json({ user: currentUser.format(), token });
+		} catch (error) {
+			next(error);
+		}
+	},
+	updateUser: async (req, res, next) => {
+		try {
+			var user = req.body.user;
+			console.log("BACKEND BODY.USER", user);
 
-      if (!user || !isValidUser(user)) {
-        return res.status(400).json({ massage: "Invalid Input" });
-      }
+			if (!user) {
+				return res.status(400).json({ massage: "Invalid User" });
+			}
 
-      var currentUser = await User.findById(req.userId);
+			var currentUser = await User.findById(req.userId);
 
-      currentUser.email = user.email;
-      currentUser.password = user.password;
-      currentUser.name = user.name;
+			// console.log(currentUser, "Before adding Password");
 
-      var updatedUser = await currentUser.save();
+			// //if user has not changed password, assign the previous password again to user to pass isValidUser validator
+			// if (user.password && !user.password === "") {
+			// 	currentUser.password = user.password;
+			// } else {
+			// 	user.password = currentUser.password;
+			// }
 
-      res.send({ user: updatedUser });
-    } catch (error) {
-      next(error);
-    }
-  },
-  getCurrentUser: async (req, res, next) => {
+			// console.log(user, "After adding Password");
 
-    try {      
-      var userId = req.userId;
+			// //run isValidUser and if not valid user return error
 
-      var user = await User.findById(userId);
-      
-      if (!user) {
-        res.status(404).send({ massage: "User not found" });
-      }
-  
-      res.send({  user : user.format()}) ;
-    } catch (error) {
-        next(error); 
-    }
+			// if (!isValidUser(user)) {
+			// 	return res.status(400).json({ massage: "Invalid Input" });
+			// }
 
-  },
+			// //var updatedUser = await currentUser.save();
+			// var updatedUser = await currentUser.save();
+
+			//newChange
+
+			//real change
+			if (user.email) {
+				currentUser.email = user.email;
+			}
+			if (user.password) {
+				currentUser.password = user.password;
+			}
+			if (user.name) {
+				currentUser.name = user.name;
+			}
+			var updatedUser = await currentUser.save();
+
+			res.send({ user: updatedUser });
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	getCurrentUser: async (req, res, next) => {
+		try {
+			var userId = req.userId;
+
+			var user = await User.findById(userId);
+
+			if (!user) {
+				res.status(404).send({ massage: "User not found" });
+			}
+
+			res.send({ user: user.format() });
+		} catch (error) {
+			next(error);
+		}
+	},
 };
+
+//isValueToBeUpdatedValid
